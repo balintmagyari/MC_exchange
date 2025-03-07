784edbbe3193cbed52cdcb520350d14555e9a8af
balintmagyari <balint.magyari@unina.it>
2025-03-07 10:37:13 +0100

    Bond exchange function updated

--- src/MC_exchange/exchange.py
+++ src/MC_exchange/exchange.py
@@ -2,28 +2,72 @@ import numpy as np
 import warnings
 import random
 
+from typing import Union
+
 from mpi4py import MPI
 from .calculations import calculate_distance_pbc, calculate_fene_potential, calculate_lj_potential
 
 
 def perform_bond_exchange(sticker_neighbor_list: dict, 
                           bonds: np.ndarray,
-                          atoms: np.ndarray, 
+                          atoms: np.ndarray,
                           box_dims: np.ndarray,
+                          T: float,
                           cut_off: float | None = None,
                           P_coeff: float = 1.0,
                           kB: float = 1.0,
-                          T: float = 1.0,
-                          comm: MPI.Intracomm = MPI.COMM_WORLD
-                          ) -> tuple[dict, dict]:
-    
-    # TODO: adjust T so that it accepts argument from LAMMPS MD simulation. Delete default value.
+                          comm: MPI.Intracomm = MPI.COMM_WORLD,
+                          return_stats: bool = False
+                          ) -> Union[tuple[dict, dict], tuple[dict, dict, int, int, int, int]]:
+    """
+    Evaluate bond exchange dynamics on the local process, gather combined data on which
+    bonds to delete and create, and broadcast that combined data to all processes.
 
+    If return_stats = True, statistics about the number of exchanges is returned.
+
+    Parameters
+    ---------
+    sticker_neighbor_list : dict
+        Dictionary containing the local neighbor list on the current process.
+    bonds : np.ndarray
+        Bond data organized as a structured numpy array with columns: ['type', 'atom 1', 'atom 2'].
+    atoms : np.ndarray
+        Atom data organized as a structured numpy array with columns: ['id', 'type', 'mol', 'x', 'y', 'z'].
+    box_dims : np.ndarray
+        Box dimensions in the form: [xlo, xhi, ylo, yhi, zlo, zhi].
+    T : float
+        Current temperature of the simulation.
+    cut_off : float | None
+        Cut-off distance for bond exchange dynamics to be considered. If None, the cut-off distance from the
+        neighbor list is used. Default is None.
+    P_coeff : float
+        Defines the maximum of the range between 0 and P_coeff, from which a random number is drawn during the
+        Monte Carlo exchange. Default is 1.0.
+    kB : float
+        Boltzmann's constant. Default is 1.0.
+    comm : MPI.Intracomm 
+        MPI communicator. Default is MPI.COMM_WORLD.
+    return_stats : bool
+        Whether to return bond exchange statistics (number of exchanges, etc.). Default is False.
+
+    Returns
+    -------
+    tuple[dict, dict] | tuple[dict, dict, int, int, int, int]
+
+    Depending on the value of return_stats. If return_stats = False, bonds_to_delete and bonds_to_create dictionaries are returned. 
+    If return_stats = True, bonds_to_delete, bonds_to_create, total_N_possible, total_N_exchanges, total_N_deltaU_exchanges, total_N_MC_exchanges are returned.
+
+    total_N_possible : Total number of possible bond exchanges based on distance criterion.
+    total_N_exchanges : Total number of actual bond exchanges to be perfomed. total_N_exchanges = total_N_deltaU_exchanges + total_N_MC_exchanges.
+    total_N_deltaU_exchanges : Total number of bond exchanges performed due to a reduces potential.
+    total_N_MC_exchanges : Total number of bond exchanges performed through Monte Carlo evaluation.
+    """
+    
     # Counters for statistics
-    N_possible = 0          # Possible total number of exchanges given solely the distance criterion.
-    N_exchanges = 0         # Number of actualy exchanges happening. N_exchanges = N_deltaU_exchanges + N_MC_exchanges
-    N_deltaU_exchanges = 0  # Number of exchanges due to reduction in interaction potential.
-    N_MC_exchanges = 0      # Number of exchanges due to random probability acceptance.
+    N_possible = 0              # Possible total number of exchanges given solely the distance criterion.
+    N_exchanges = 0             # Number of actualy exchanges happening. N_exchanges = N_deltaU_exchanges + N_MC_exchanges
+    N_deltaU_exchanges = 0      # Number of exchanges due to reduction in interaction potential.
+    N_MC_exchanges = 0          # Number of exchanges due to random probability acceptance.
 
     already_exchanged_atoms = []    # List used to prevent the same pair of atoms taking part in bond exchange more than once
     bonds_to_delete = {}            # Final dictionary containing atom1 and atom2 as key: value pairs between which bonds should be created after bond exchange
@@ -216,8 +260,8 @@ def perform_bond_exchange(sticker_neighbor_list: dict,
                 ran = random.uniform(0, P_coeff)
                 if P_accept >= ran:
                     N_MC_exchanges += 1
-                    # print('Bond exchange happens due to Metropolis acceptance criterion.')
                     bond_exchange = True
+                    # print('Bond exchange happens due to Metropolis acceptance criterion.')
 
             # Swapping bonds if bond exchange was deemed plausible
             if bond_exchange:
@@ -232,29 +276,8 @@ def perform_bond_exchange(sticker_neighbor_list: dict,
                 already_exchanged_atoms.append(atom_c)
                 already_exchanged_atoms.append(atom_d)
 
-                # print(f'''OLD BONDS:
-                #       Bond ID | Atom 1 | Atom 2
-                #       {bond_1} | {atom_main} | {atom_c}
-                #       {bond_2} | {neighbor_id} | {atom_d}''')
-                
-                # print(f'''NEW BONDS:
-                #       Bond ID | Atom 1 | Atom 2
-                #       {bond_1} | {new_bond1_atom_1} | {new_bond1_atom_2}
-                #       {bond_2} | {new_bond2_atom_1} | {new_bond2_atom_2}\n''')
-
-                # print(f'Atom IDs involved in exchange: {new_bond1_atom_1} {new_bond1_atom_2} {new_bond2_atom_1} {new_bond2_atom_2}')
-                # print(f'Bond IDs involved in exchange: {bond_1}, {bond_2} \n')
-
-                # print(f'------------------------------------------------------------------\n')
-
                 N_exchanges += 1        
 
-        #     if N_possible == 1:
-        #         break
-
-        # if N_possible == 1:
-        #     break
-        
     # print(f"\nTotal number of POSSIBLE bond exchanges given distance criterion: {N_possible}", flush=True)
     # print(f"Total number of ACTUAL bond exchanges performed: {N_exchanges}", flush=True)
     # print(f"Number of bond exchanges due to reduced potential: {N_deltaU_exchanges}", flush=True)
@@ -265,6 +288,13 @@ def perform_bond_exchange(sticker_neighbor_list: dict,
     gathered_bonds_to_delete = comm.gather(bonds_to_delete, root=0)
     gathered_bonds_to_create = comm.gather(bonds_to_create, root=0)
 
+    # Summing bond exchange statistics on root process
+    if return_stats:
+        total_N_possible = comm.reduce(N_possible, op=MPI.SUM, root=0)
+        total_N_exchanges = comm.reduce(N_exchanges, op=MPI.SUM, root=0)
+        total_N_deltaU_exchanges = comm.reduce(N_deltaU_exchanges, op=MPI.SUM, root=0)
+        total_N_MC_exchanges = comm.reduce(N_MC_exchanges, op=MPI.SUM, root=0)
+    
     mpi_rank = comm.Get_rank()
     if mpi_rank == 0:
         complete_bonds_to_delete = {}
@@ -279,7 +309,24 @@ def perform_bond_exchange(sticker_neighbor_list: dict,
         complete_bonds_to_delete = None
         complete_bonds_to_create = None
 
+        if return_stats:
+            total_N_possible = None
+            total_N_exchanges = None
+            total_N_deltaU_exchanges = None
+            total_N_MC_exchanges = None
+
+    # Broadcasting bonds to delete/create dictionaries
     complete_bonds_to_delete = comm.bcast(complete_bonds_to_delete, root=0)
     complete_bonds_to_create = comm.bcast(complete_bonds_to_create, root=0)
 
-    return complete_bonds_to_delete, complete_bonds_to_create
+    # Broadcasting bond exchange statistics
+    if return_stats:
+        total_N_possible = comm.bcast(total_N_possible, root=0)
+        total_N_exchanges = comm.bcast(total_N_exchanges, root=0)
+        total_N_deltaU_exchanges = comm.bcast(total_N_deltaU_exchanges, root=0)
+        total_N_MC_exchanges = comm.bcast(total_N_MC_exchanges, root=0)
+
+    if return_stats:
+        return complete_bonds_to_delete, complete_bonds_to_create, total_N_possible, total_N_exchanges, total_N_deltaU_exchanges, total_N_MC_exchanges
+    else:
+        return complete_bonds_to_delete, complete_bonds_to_create
